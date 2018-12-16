c32f288d78e8fe11ca985e0ff62d81d117730496
kamilwu <kami36@poczta.fm>
2018-12-16 16:21:12 +0100

    Completed day 4

--- day4.py
+++ day4.py
@@ -5,8 +5,6 @@ import re
 def choose_guard(history, strategy):
     total_asleep = defaultdict(int)
     minutes = {}
-    guard = -1
-    begin, end = -1, -1
     for tm, what in history:
         if 'begins shift' in what:
             guard = re.findall(r'\d+', what)[0]
@@ -20,13 +18,18 @@ def choose_guard(history, strategy):
                 minutes[guard] = [0] * 60
             for minute in range(begin, end):
                 minutes[guard][minute] += 1
-    best_guard = max(total_asleep, key=total_asleep.get)
-    best_minute = minutes[best_guard].index(max(minutes[best_guard]))
 
     if strategy == 1:
+        best_guard = max(total_asleep, key=total_asleep.get)
+        best_minute = minutes[best_guard].index(max(minutes[best_guard]))
         return int(best_guard) * best_minute
-    else:
-        return 0
+    elif strategy == 2:
+        best = 0, 0, 0
+        for guard, his_minutes in minutes.items():
+            the_most = max(his_minutes)
+            if the_most > best[2]:
+                best = int(guard), his_minutes.index(the_most), the_most
+        return best[0] * best[1]
 
 if __name__ == '__main__':
     history = []
